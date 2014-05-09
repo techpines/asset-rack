6ffd95bd0f9b71911641563e3d565637abdefaf1
Ryan Billingsley <ryanbbillingsley@gmail.com>
2014-05-08 20:44:05 -0400

    Adding register call for Coffee-script

--- switch.js
+++ switch.js
@@ -3,6 +3,7 @@ try {
     module.exports = require('./compiled');
 } catch(error) {
     //require('./node_modules/coffee-script');
-    require('coffee-script');
+    var CoffeeScript = require('coffee-script');
+    CoffeeScript.register();
     module.exports = require('./lib');
 }
